fb9cfe7a3a0f2c27ac879eb8c44886596ff9dfd8
MariaFernandaGiron <60407668+MariaFernandaGiron@users.noreply.github.com>
2020-07-27 21:58:01 -0600

    Adquisición señales
    Actualización del algoritmo de adquisición de señales

--- EMG/código/matlab/leer_datos.m
+++ EMG/código/matlab/leer_datos.m
@@ -1,6 +1,4 @@
-%% PRUEBA ADQUISICIÓN DE DATOS E IDENTIFICACIÓN DE ACTIVIDAD
-%2 SEGUNDOS DE CERO MOV. EN TIEMPO REAL
-
+%% ALGORITMO DE ADQUISICIÓN DE SEÑALES sEMG E IDENTIFICACIÓN DE ACTIVIDAD
 close all;clc;
 
 %Inicializar puerto serial
@@ -9,96 +7,76 @@ delete(instrfind({'Port'},{puerto}));
 pserial=serial(puerto,'BaudRate',115200);
 fopen(pserial);
 
-srate = 1000;     %frec. muestreo
-t = 1;            %tiempo total
-t_m = 0.80;       %tiempo mov.
-t_r = 0.20;       %tiempo sin mov.
-m = t*srate;      %no. muestras totales
-m_m = t_m*srate;  %no. muestras mov.
-m_r = t_r*srate;  %no. muestras sin mov.
+srate = 1000;     %Frec. muestreo
+t_inicio = 1000;  %Tiempo inicial sin mov. en ms
+t = 1;            %Tiempo total
+t_m = t*0.85;     %Tiempo mov.
+t_r = t*0.15;     %Tiempo sin mov.
+m = t*srate;      %No. muestras totales
+m_m = t_m*srate;  %No. muestras mov.
+m_r = t_r*srate;  %No. muestras sin mov.
 
+cont = 1;         %Contador no. de muestras
+cont_r = m_r;     %Contador no. de muestras anteriores sin mov.
+cont_f = 0;       %Contador no. grabaciones
+grab = 2;         %No. grabaciones
 
-cont = 1;       %contador no. de muestras
-cont_s = 1;     %contador no. muestras sin mov.
-cont_r = m_r;   %contador no. de muestras anteriores sin mov.
-cont_f = 0;     %contador detener comunicación
-grab = 2;       %no. grabaciones
+data = zeros(1,m_m);    %Array para almacenar datos con movimiento
+data_c = zeros(1,m);    %Array para almacenar datos centrados 
+data_r = zeros(1,m_r);  %Array para almacenar un tiempo sin mov.
+data_f = [];            %Array para almacenar datos filtro pasa bandas
+data_n = [];            %Array para almacenar datos filtro notch
 
-data = zeros(1,m_m);    %array para almacenar datos con movimiento
-data_c = zeros(1,m);    %array para almacenar datos centrados
-data_s = zeros(1,2000); %array para almacenar datos del puerto serial 
-data_r = zeros(1,m_r);  %array para almacenar 0.25s sin mov.
-data_f = [];            %array para almacenar datos filtro pasa bandas
-data_n = [];            %array para almacenar datos filtro notch
+almacenar = zeros(grab,m);                 %Almacenar todas las corridas
 
-almacenar = zeros(grab,m);      %almacenar todas las corridas
-
-v = 0;    
-volt = 0;     
-s = 0;               %bandera inicio ciclo 
-r = 0;               %bandera para los 2 sec de ruido
-
-F_pb = filtroPasaBandasML(srate,50,150);   %Diseñar filtro pasa bandas         
+F_pb = filtroPasaBandasML(srate,50,200);   %Diseñar filtro pasa bandas         
 F_notch = filtroStopBandML(srate);         %Diseñar filtro notch 
 
-th = 0;
-ga = 0.1;
-b_act = 0; 
-x = 1;  %solo para probar
+%Inicializar variables
+v = 0;    
+volt = 0;     
+ga = 0.05;                                 %Tolerancia threshold
+b_act = 0;                                 %Bandera para detectar actividad
 
-%DETALLES GRÁFICA
-% figure(1); clf;
-% h = plot(1:m,zeros(1,m));
-% xlim([0,m]);
+%Detalles gráfica
+figure(1); clf;
+h = plot(1:m,zeros(1,m));
+xlim([0,m]);
 
-promp = 'Presiona 0 + enter para inciar';
-x = input(promp)                           %Recibir dato del usuario para inciar
-fwrite(pserial,107,'uint8');               %Iniciar el envío de datos desde arduino
+th = identificacion(pserial,t_inicio);     %Identificar el valor del threshold
 
-while x == 0 && cont_f < grab
-    v = fscanf(pserial,'%d');              %leer datos puerto serial
-    volt = v(1)*5/1024;
-    data_r(cont_r) = volt;
+while cont_f < grab
+    v = fscanf(pserial,'%d');              %Leer datos puerto serial
+    volt = v(1)*5/1024;                    %Convertir de 0-5V
+    data_r(cont_r) = volt;                 %Almacenar una ventada de muestras anteriores al mov.
     cont_r = cont_r - 1;
     
-    if cont_s < 2001 && r == 0 
-        data_s(cont_s) = volt;
-        cont_s = cont_s + 1;                %aumentar contador      
-    elseif cont_s == 2001 && r == 0         %capturar los dos segundos sin mov. para encontrar th   
-        th = max(abs(data_s));
-        cont_s = 1;
-        r = 1;
-        disp("DONE");
-    end
-    
-    if cont_r == 0                          %reset el contador para las muestras anteriores al mov.
-        cont_r = m_r;
-    end 
-   
-    if (volt > (th + ga) && b_act == 0 && r == 1
-        %activar bandera de inicio de actividad
+    if volt > (th + ga) && b_act == 0      
+        %Activar bandera de inicio de actividad
         b_act = 1
     end
     
-    if b_act == 1                                                   %guardo la señal activa
-        data(cont) = volt;                                          %convertir de 0-5 voltios 
-        cont = cont +1;                                             %aumentar contador
-        if(cont == (m_m+1))                                         %evaluar longitud del contador
-            data_c = [data_r - mav(data_r),data - mav(data)];       %centrar datos
-            data_f = filter(F_pb, data_c);                          %aplicar filtro pasa bandas
-            data_n = filter(F_notch, data_f);                       %aplicar filtro notch
-            cont = 1;                                               %reiniciar contador
-            cont_f = cont_f + 1;                                    %aumentar no. co
-            b_act = 0;                                              %reiniciar bandera de actividad
+    if b_act == 1                                                   
+        data(cont) = volt;                                          %Guardo la señal activa
+        cont = cont +1;                                            
+        if(cont == (m_m+1))                                         %Evaluar longitud del contador
+            data_c = [data_r - mav(data_r),data - mav(data)];       %Centrar datos
+            data_f = filter(F_pb, data_c);                          %Aplicar filtro pasa bandas
+            data_n = filter(F_notch, data_f);                       %Aplicar filtro notch
+            cont = 1;                                               %Reiniciar contador
+            cont_f = cont_f + 1;                                    %Aumentar contador para no. de grabaciones
+            b_act = 0                                               %Reiniciar bandera de actividad
             almacenar(cont_f,:) = data_n;                           %Almacenar todas las corridas 
-            %plot(data_n);
-            %h.YData = data_n;
-            %drawnow limitrate
+            h.YData = data_n;                                       %Actualizar gráfica
+            drawnow limitrate
         end
     end
-        
+    
+    if cont_r == 0                          %Reset el contador para las muestras anteriores al mov.
+        cont_r = m_r;
+    end 
 end
 
 fclose(pserial);
 delete(pserial);
-%save('corridas.mat', 'almacenar');           %almacenar todas las señales filtradas en un .mat
+%save('corridas.mat', 'almacenar');          %Almacenar todas las señales filtradas en un .mat
